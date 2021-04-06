c954ec7306027fdc1a6b714654ee6bc1d02337c1
zjl222666 <54488861+zjl222666@users.noreply.github.com>
2021-04-06 21:47:36 +0800

    footer更新
    搞定页脚

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-layout>
+      <a-layout-content>
+        <router-view/>
+      </a-layout-content>
+      <a-layout-footer style="background: rgb(46, 46, 46)">     <vfooter/>    </a-layout-footer>
+    </a-layout>
+    <router-view/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
+import Vfooter from './components/Vfooter.vue'
+export default{
   components: {
-    HelloWorld
+    Vfooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
