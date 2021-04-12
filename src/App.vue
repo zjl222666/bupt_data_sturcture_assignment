675424e6e81a1dcf44ba1e0aa60513637af9ac10
zjl222666 <54488861+zjl222666@users.noreply.github.com>
2021-04-12 20:57:01 +0800

    整体布局
    确定了整体布局框架并实现了部分

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <a-layout id="components-layout-demo-custom-trigger">
-    <a-layout>
     <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
       <div class="logo"></div>
       <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
@@ -9,46 +8,69 @@
           <span>校区一</span>
         </a-menu-item>
         <a-menu-item key="2">
-          <a-icon type="video-camera" />
+          <a-icon type="layout" />
           <span>校区二</span>
         </a-menu-item>
         <a-menu-item key="3">
-          <a-icon type="upload" />
+          <a-icon type="environment" />
           <span>跨校区导航</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="() => (collapsed = !collapsed)"
-        />
+      <a-layout>
+      <a-layout-header style="background: #fff; padding: 10px; width=100%">
+        <a-row type="flex" justify="space-around" align="middle">
+          <a-col span="9">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="() => (collapsed = !collapsed)"
+          />
+          </a-col>
+          <a-col span="7">  <vheader/></a-col>
+          <a-col span="4">  <a-icon type="ellipsis" class="trigger" @click="() => (drawopened = !drawopened)"/> </a-col>
+        </a-row>
       </a-layout-header>
       <a-layout-content
         :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
       >
         Content
       </a-layout-content>
+      <a-layout-footer style=" background: rgb(46, 46, 46);"> <vfooter/></a-layout-footer>
     </a-layout>
-    <a-layout-footer style=" background: rgb(46, 46, 46);"> <vfooter/></a-layout-footer>
+    <a-drawer
+      title="路线规划"
+      placement="right"
+      :closable="false"
+      :visible="drawopened"
+      :after-visible-change="afterVisibleChange"
+      @close="() => (drawopened = !drawopened)"
+    >
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-drawer>
   </a-layout>
+  
 </template>
 <script>
 import Vfooter from './components/Vfooter.vue'
+import Vheader from './components/Vheader.vue'
 export default{
   components: {
-    Vfooter
+    Vfooter,
+    Vheader
   },
   data() {
     return {
       collapsed: false,
+      drawopened: false,
     };
   },
 }
 </script>
 <style>
-#components-layout-demo-custom-trigger .trigger {
+.trigger {
   font-size: 18px;
   line-height: 64px;
   padding: 0 24px;
@@ -56,11 +78,11 @@ export default{
   transition: color 0.3s;
 }
 
-#components-layout-demo-custom-trigger .trigger:hover {
+.trigger:hover {
   color: #1890ff;
 }
 
-#components-layout-demo-custom-trigger .logo {
+.logo {
   height: 32px;
   background: rgba(255, 255, 255, 0.2);
   margin: 16px;
---
+++ src/components/Vheader.vue
@@ -0,0 +1,11 @@
+<template>
+    <a-input-search placeholder="输入搜索地址"/>
+</template>
+<script>
+export default{
+    name: "myheader"
+}
+</script>
+<style scoped>
+
+</style>
