8147c1cb4845c1301b5e7c007b138a033b1d73df
zjl222666 <54488861+zjl222666@users.noreply.github.com>
2021-05-25 14:42:00 +0800

    地图更新

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -11,34 +11,57 @@ export default {
   data () {
     return {
       mapName: "校区一",
-      node: [],
-      links: [],
-      searchNode: [],
-      searchLinks: [],
-      nowPoint: 0,
-      mypos:[],
-      minx: 0,
+      node: [], //地图点
+      links: [], //地图边
+      searchNode: [], //导航路径被加载的点
+      searchLinks: [], //导航路径被加载的边
+      nowPoint: 0, //模拟导航当前走到的点
+      mypos:[], //我的位置
+      minx: 0, //视图范围
       maxx: 30,
       miny: 0,
       maxy: 30,
-      inGuide: false,
-      rate: 0.05
+      inGuide: false, //是否正在导航
+      rate: 0.05 //导航的速率
     }
+  },
+  props: {
+       nodeValue:[], //导航路径点
   },
   mounted(){
     this.myChart = this.$echarts.init(document.getElementById('myChart'));
     this.getMap();
-    this.drawGuide();
+  },
+  watch: {
+      nodeValue(newVal) {
+          this.searchNode=[]
+          this.searchLinks=[]
+          let count = -1
+          newVal.forEach(nodeVal => {
+              this.searchNode.push({
+                  name: (++count).toString,
+                  symbol: "none",
+                  value: nodeVal
+              })
+          })
+          for(let i = 0; i< count; i++) {
+              this.searchLinks.push({
+                  source: i.toString,
+                  target: (i+1).toString
+              })
+          }
+          this.drawGuide()
+      }
   },
   methods: {
-    startGuide() {
-        if(this.nowPoint==0) {
-            this.mypos = this.searchNode[0].value;
-            this.nowPoint++;
-        }
+    startGuide() { //模拟导航函数，开始后若条件允许则会不断回调，可通过inGuide来实现暂停
         if(this.nowPoint>=this.searchNode.length) {
             console.log("over!");
             return;
+        }
+        if(this.nowPoint==0) {
+            this.mypos = this.searchNode[0].value;
+            this.nowPoint++;
         }
         let tmpX = this.searchNode[this.nowPoint].value[0];
         let tmpY = this.searchNode[this.nowPoint].value[1];
@@ -68,25 +91,18 @@ export default {
         }
     },
     drawGuide() {
-        this.inGuide = true;
-        this.nowPoint = 0;
-        this.$http.get('/guideRoute.json').then(res =>{
-            this.searchNode = res.data.node
-            this.searchLinks = res.data.links
-            console.log(this.searchNode)
-            this.myChart.setOption({
-            series: [{
-                id: "guideContent",
-                type: "graph",
-                data: this.searchNode,
-                links: this.searchLinks,
-                coordinateSystem: 'cartesian2d',
-                lineStyle: {
-                    color: '#FFF',
-                    width: 5
-                }
+        this.myChart.setOption({
+        series: [{
+            id: "guideContent",
+            type: "graph",
+            data: this.searchNode,
+            links: this.searchLinks,
+            coordinateSystem: 'cartesian2d',
+            lineStyle: {
+                color: '#FFF',
+                width: 5
+            }
             }]
-        })
         })
     },
     max(x,y) {
